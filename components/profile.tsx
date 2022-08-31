e7794f0ecd9f4ce73cd90bd005a9107c478faad5
Hamish Bray <hamish.bray@gmail.com>
2022-08-31 10:45:02 +1000

    profile update

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
-const Profile = () => <Image src="/images/profile.jpg" height={144} width={144} alt="me..." />;
+const Profile = () => <Image src="/images/profile.png" height={144} width={144} alt="me..." />;
 
 export default Profile;
